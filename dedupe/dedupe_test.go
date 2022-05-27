ce975b51e46faa079f22215accd23cf81c50d41a
Sander Skjulsvik <sander.skjulsvik@outlook.com>
2022-05-27 22:16:22 +0200

    started on the dedupe test

---
+++ dedupe/dedupe_test.go
@@ -0,0 +1,17 @@
+package dedupe_test
+
+// func TestFindDupes(t *testing.T) {
+// 	root := "./tmp_test_dir"
+// 	BuildFolderTree(root)
+// 	defer cleanup(root)
+// 	files := dedupe.GetFiles(root)
+// 	dupes := dedupe.Dupes{}
+// 	dupes.BuildDuplicates(files)
+// 	dupes.PrintDuplicates()
+// 	// files := dedupe.GetFiles(root)
+// 	// expected := dedupe.Dupes{
+// 	// m := make(map(strings))
+// 	// }
+
+// 	// cleanup(root)
+// }
