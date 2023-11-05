6cc6aef355d100b06870776391edc66f18791bc3
sander-skjulsvik <sander.skjulsvik@pexip.com>
2023-11-05 15:05:24 +0100

    Cleaned up producerConsumer implementation

--- dupes/lib/producerConsumer/main.go
+++ dupes/lib/producerConsumer/main.go
@@ -1,13 +1,10 @@
 package dupes
 
 import (
-	"flag"
 	"fmt"
 	"io/fs"
-	"log"
 	"os"
 	"path/filepath"
-	"sync"
 
 	"github.com/sander-skjulsvik/tools/dupes/lib/common"
 )
@@ -19,50 +16,45 @@ func visit(path string, f os.FileInfo, err error) (string, error) {
 	return path, nil
 }
 
-func directoryWalker(root string, files chan<- common.File) {
+func directoryWalker(root string, filePaths chan<- string) {
 	filepath.Walk(root, func(path string, info fs.FileInfo, err error) error {
 		_, err = visit(path, info, err)
 		if err != nil {
 			return err
 		}
-		files <- path
+		filePaths <- path
 		return nil
 	})
 }
 
-func processer(dupes sync.Map, files <-chan string) {
+func processor(filePaths <-chan string) (common.Dupes, error) {
+	dupes := common.Dupes.New(common.Dupes{})
 
+	for filePath := range filePaths {
+		dupes.Append(filePath)
+	}
+
+	return dupes, nil
 }
 
-func main() {
-	flag.Parse()
-	src := flag.Arg(0)
-	var files chan common.File
-	directoryWalker(src, files)
-	processer(files)
-	storer(files)
+func presenter(dupes common.Dupes) {
+	dupes.Print()
+}
 
-	if d, ok := dupes[hash]; !ok {
-		// If file hash has not been found yet
-		dupe := Dupe{
-			hash:  hash,
-			Paths: []*string{&path},
-		}
+func Run(src string) {
 
-		dupes[hash] = &dupe
-	} else {
-		d.Paths = append(d.Paths, &path)
-	}
+	// Find the files
+	var filePaths chan string
+	directoryWalker(src, filePaths)
+
+	// Process files
+	dupes, err := processor(filePaths)
 	if err != nil {
-		log.Fatalf("Failed to walk src: %s, with err: %s", src, err.Error())
+		fmt.Printf("Failed to process directory: %s\n", err)
+		panic("Stopping because off processing dir error")
 	}
-	dupes := map[string]*Dupe{}
 
-	for _, dupe := range dupes {
-		fmt.Printf("sha256:%s \n", dupe.hash)
-		for _, path := range dupe.Paths {
-			fmt.Printf("    %s \n", *path)
-		}
-		fmt.Println("")
-	}
+	// Present the result (?)
+	presenter(dupes)
+
 }
