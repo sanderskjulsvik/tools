fddf4157f0ec6d8bc35782249d3de0456360b530
sander skjulsvik <sander.skjulsvik@pexip.com>
2024-05-07 20:39:17 +0200

    Added CompareDirs and child functions

--- compareDirs/lib/lib.go
+++ compareDirs/lib/lib.go
@@ -3,35 +3,46 @@ package comparedirs
 import (
 	"fmt"
 
+	"github.com/sander-skjulsvik/tools/dupes/lib/common"
 	producerconsumer "github.com/sander-skjulsvik/tools/dupes/lib/producerConsumer"
 )
 
 // CompareDirs compares two directories
-func CompareDirs(dir1, dir2 string, mode) {
-	fmt.Printf("Comparing directories: %s and %s\n", dir1, dir2)
-	dir1dupes := producerconsumer.Run(dir1)
-	dir2dupes := producerconsumer.Run(dir2)
+func CompareDirs(dir1, dir2 string, mode string) (*common.Dupes, error) {
+	var newD *common.Dupes
 	switch mode {
-	case "both":
-		both := onlyInBoth(dir1dupes, dir2dupes)
-		both.Present(true)
-
 	// Show dupes that is present in both directories
-
-	// Show dupes that is only present in dir1
-
-	// Show dupes that is only present in dir2
+	case "OnlyInboth":
+		newD = OnlyInboth(dir1, dir2)
+	// Show dupes that is only present in first
+	case "onlyInFirst":
+		newD = OnlyInFirst(dir1, dir2)
+	case "all":
+		newD = All(dir1, dir2)
+	default:
+		return nil, fmt.Errorf("unknown mode: %s", mode)
+	}
+	return newD, nil
 }
 
-
-func onlyInBoth(dupes1 *common.Dupes, dupes2 *common.Dupes) *common.Dupes {
-
+// OnlyInboth returns dupes that is present in both directories
+func OnlyInboth(path1, path2 string) *common.Dupes {
+	d1 := producerconsumer.Run(path1)
+	d2 := producerconsumer.Run(path2)
+	return d1.OnlyInBoth(d2)
 }
 
-func onlyInFirst(dupes1 *common.Dupes, dupes2 *common.Dupes) *common.Dupes {
-
+// OnlyInFirst returns dupes that is only present in first directory
+func OnlyInFirst(path1, path2 string) *common.Dupes {
+	d1 := producerconsumer.Run(path1)
+	d2 := producerconsumer.Run(path2)
+	return d1.OnlyInSelf(d2)
 }
 
-func onlyDestinct(dupes1 *common.Dupes, dupes2 *common.Dupes) *common.Dupes {
-
+// All returns all dupes in both directories
+func All(path1, path2 string) *common.Dupes {
+	d1 := producerconsumer.Run(path1)
+	d2 := producerconsumer.Run(path2)
+	d1.AppendDupes(d2)
+	return d1
 }
