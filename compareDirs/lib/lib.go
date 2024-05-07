74bc998067768e31b9af4f02f664251bde0204ff
sander skjulsvik <sander.skjulsvik@pexip.com>
2024-05-07 20:49:54 +0200

    Cleaned up compare dirs

--- compareDirs/lib/lib.go
+++ compareDirs/lib/lib.go
@@ -1,29 +1,9 @@
 package comparedirs
 
 import (
-	"fmt"
-
 	"github.com/sander-skjulsvik/tools/dupes/lib/common"
 	producerconsumer "github.com/sander-skjulsvik/tools/dupes/lib/producerConsumer"
 )
-
-// CompareDirs compares two directories
-func CompareDirs(dir1, dir2 string, mode string) (*common.Dupes, error) {
-	var newD *common.Dupes
-	switch mode {
-	// Show dupes that is present in both directories
-	case "OnlyInboth":
-		newD = OnlyInboth(dir1, dir2)
-	// Show dupes that is only present in first
-	case "onlyInFirst":
-		newD = OnlyInFirst(dir1, dir2)
-	case "all":
-		newD = All(dir1, dir2)
-	default:
-		return nil, fmt.Errorf("unknown mode: %s", mode)
-	}
-	return newD, nil
-}
 
 // OnlyInboth returns dupes that is present in both directories
 func OnlyInboth(path1, path2 string) *common.Dupes {
--- compareDirs/main.go
+++ compareDirs/main.go
@@ -4,12 +4,16 @@ import (
 	"flag"
 	"fmt"
 	"os"
+
+	comparedirs "github.com/sander-skjulsvik/tools/compareDirs/lib"
+	"github.com/sander-skjulsvik/tools/dupes/lib/common"
 )
 
 func main() {
 	// Define command-line flags
 	dir1 := flag.String("dir1", "", "First directory path")
 	dir2 := flag.String("dir2", "", "Second directory path")
+	mode := flag.String("mode", "All", "Mode to run in")
 
 	// Parse command-line flags
 	flag.Parse()
@@ -20,5 +24,18 @@ func main() {
 		os.Exit(1)
 	}
 
-	CompareDirs(*dir1, *dir2)
+	var newD *common.Dupes
+	switch *mode {
+	// Show dupes that is present in both directories
+	case "OnlyInboth":
+		newD = comparedirs.OnlyInboth(*dir1, *dir2)
+	// Show dupes that is only present in first
+	case "onlyInFirst":
+		newD = comparedirs.OnlyInFirst(*dir1, *dir2)
+	case "all":
+		newD = comparedirs.All(*dir1, *dir2)
+	default:
+		panic(fmt.Errorf("unknown mode: %s", mode))
+	}
+	newD.Present(false)
 }
