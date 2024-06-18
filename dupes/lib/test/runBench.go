b55087f695c2659dbcc11e79d8009a6bb2d7792a
sander skjulsvik <sander.skjulsvik@pexip.com>
2024-06-18 23:15:21 +0200

    continue on bensh

--- dupes/lib/test/runBench.go
+++ dupes/lib/test/runBench.go
@@ -1,6 +1,8 @@
 package test
 
 import (
+	"crypto/rand"
+	"fmt"
 	"os"
 	"testing"
 
@@ -23,11 +25,32 @@ func TestBenchLargeFiles() {}
 func TestBenchMixed() {}
 
 // doing single lvl, assuming deep files does not make a difference
-func generateFiles(path string, numberOfFiles, sizeOfFilesB, groupSizes int) {
+func generateFiles(path string, numberOfFiles, sizeOfFilesB, groupSize int) {
+	for i := 0; i < numberOfFiles; i++ {
+		createFileRandom(fmt.Sprint(i), sizeOfFilesB)
+		for range groupSize - 1 {
+			file.copy
+			i++
+		}
+	}
 }
 
 func createFileRandom(path string, size int) {
-	fp := os.Open(path)
+	chunkSize := size
+	if size < 1e4 {
+		chunkSize = 1e4
+	}
+	fp, err := os.Open(path)
+	check(err)
 	defer fp.Close()
-	fp.Write([]byte("This is the common content for all files in the folder."))
+	written := 0
+	for written < size {
+		chunk := make([]byte, chunkSize)
+		rand.Read(chunk)
+		w, writeErr := fp.Write(chunk)
+		written += w
+		if writeErr != nil {
+			check(fmt.Errorf("Error writing to file: %v", writeErr))
+		}
+	}
 }
